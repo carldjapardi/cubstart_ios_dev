7e985d0266a576897d538a95c20731d18b258538
carldjapardi <carldjapardi7@gmail.com>
2025-11-07 03:10:31 -0800

    finish disc7 api calls and networking

---
+++ disc7/GitHubDemo/ContentView.swift
@@ -0,0 +1,132 @@
+//
+//  ContentView.swift
+//  GitHubDemo
+//
+//  Created by Dylan Chhum on 10/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var user: GitUser? 
+    @State private var username: String = ""
+    @State private var isSearching: Bool = false
+
+
+    var body: some View {
+        ZStack {
+            Color(red: 0.1, green: 0.1, blue: 0.1)
+            .edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 20) {
+                HStack {
+                    TextField("Enter GitHub Username", text: $username)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    
+                    Button("Search") {
+                        Task {
+                            await fetchUser()
+                        }
+                    }
+                    .padding(.trailing)
+                }
+                
+                if isSearching {
+                    ProgressView("Loading...")
+                } else {
+                    AsyncImage(url: URL(string: user?.avatarUrl ?? "")) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .clipShape(Circle())
+                            .frame(width: 200, height: 200)
+                    } placeholder: {
+                        Circle()
+                            .foregroundColor(.secondary)
+                            .frame(width: 200, height: 200)
+                    }
+                    
+                    Text(user?.login ?? "Username Placeholder")
+                        .bold()
+                        .font(.title)
+                        .foregroundStyle(Color.white)
+                    
+                    Text(user?.bio ?? "Bio Placeholder")
+                        .padding()
+                        .foregroundStyle(Color.white)
+                }
+                
+                Spacer()
+            }
+            .padding()
+        }
+    }
+
+    // ViewModel
+    func fetchUser() async {
+        // TODO: Complete this function use try and catch blocks
+        do {
+            self.isSearching = true
+            self.user = try await getUser(username: username)
+            self.isSearching = false
+        } catch GitError.invalidData {
+            print("Failed to decode JSON data")
+        } catch GitError.invalidResponse {
+            print("Invalid Response")
+        } catch GitError.invalidURL {
+            print("Cannot convert endpoint to URL")
+        } catch {
+            print("Unexpected error")
+        }
+
+
+    }
+}
+
+//GET
+func getUser(username: String) async throws -> GitUser {
+    // TODO: Complete this function
+    // Hint: The GitHub API endpoint format is "https://api.github.com/users/{username}"
+    let endpoint = "https://api.github.com/users/\(username)"
+    
+    guard let url = URL(string: endpoint) else {
+        throw GitError.invalidURL
+    }
+    
+    let (data, response) = try await URLSession.shared.data(from: url)
+    
+    if let httpResponse = response as? HTTPURLResponse {
+        guard httpResponse.statusCode == 200 else {
+            print("Status code received is \(httpResponse.statusCode)")
+            throw GitError.invalidResponse
+        }
+    }
+    
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    do {
+        let decodedUser = try decoder.decode(GitUser.self, from: data)
+        return decodedUser
+    } catch {
+        throw GitError.invalidData
+    }
+}
+
+// Model
+struct GitUser: Codable {
+    let login: String
+    let avatarUrl: String
+    let bio: String?
+}
+
+enum GitError : Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+}
+
+#Preview {
+    ContentView()
+        .modelContainer(for: Item.self, inMemory: true)
+}
